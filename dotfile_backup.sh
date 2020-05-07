156cd4cc3e96f3ddb937777ee7b3be197c17ff59
AlviAhmed <alviahmed1234@gmail.com>
2020-05-06 23:40:05 -0400

    update

--- dotfile_backup.sh
+++ dotfile_backup.sh
@@ -30,6 +30,7 @@ printf "Backing up bash_scripts"
 cp -urv ~/bash_scripts/.  ~/Documents/Git_Projects/bash_scripts/
 
 
+
 printf "Cd into Git Repo \n" 
 cd ~/Documents/Git_Projects/dot_config_files/
 
@@ -47,3 +48,11 @@ git commit -m "update"
 
 printf "Git Push \n"    
 git push origin master
+
+printf "Backing up bash scripts to usb" 
+cp -urv ~/bash_scripts/.  /media/al/Backup Plus/linux_experimental_backup_with_terminal/al/Git_Projects/bash_scripts/
+
+printf "Backing up dot files to usb" 
+cp -urv ~/Documents/Git_Projects/dot_config_files/.  /media/al/Backup Plus/linux_experimental_backup_with_terminal/al/Git_Projects/dot_config_files/
+
+
