d3d355fe009e5c344b508ea88a14b82f4d83e82e
AlviAhmed <alviahmed1234@gmail.com>
2020-05-13 23:33:28 -0400

    removed other extra files

--- monitor_setting/DUALL.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash  
-
-xrandr --output LVDS-1 --auto --rotate normal --pos 0x0 --output VGA-1 --auto  --rotate normal --left-of LVDS-1 
-
-~/.config/polybar/launch 
--- monitor_setting/VGA1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-
-notify-send "VGA only"
-xrandr --output VGA-1 --auto --output LVDS-1 --off --rotate normal --pos 0x0 # putting all display on lvds 
-
-~/.config/polybar/launch
