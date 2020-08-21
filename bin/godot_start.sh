511df0133cceda9eb6100bc76976a0d1db97676d
AlviAhmed <alviahmed1234@gmail.com>
2020-08-20 20:07:38 -0400

    added more starter scripts

---
+++ bin/godot_start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd ~/Desktop
+./Godot_v3.2.2-stable_x11.64 
+
+
---
+++ godot_laser_game_backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+cp -urv ~/Documents/godot_projects/laser_effect_demo ~/Documents/Git_Projects/laser_game/.
