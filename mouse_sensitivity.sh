d04858b99d6ffc9840feb40b6b4eba2b3d646979
AlviAhmed <alviahmed1234@gmail.com>
2021-02-04 21:38:41 -0500

    update

--- mouse_sensitivity.sh
+++ mouse_sensitivity.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 uin=$1
 tbnid=$(xinput list | grep Trackball | sed -r 's/.*id=(\S+).*/\1/')
+tbnid1=$(xinput list | grep Full-Speed | sed -r 's/.*id=(\S+).*/\1/')
+
 if [ -z "$tbnid" ] 
 then 
 	notify-send "No track ball found" 
@@ -11,3 +13,19 @@ notify-send "Current Mouse Sensitivity: $uin"
 fi    
  
 
+# if [[ -z "$tbnid" ]] &&  [[ -z "$tbnid1" ]] 
+# then 
+#     notify-send "No track ball found"
+# elif [[ ! -z "$tbnid" ]] && [[ -z "$tbnid1" ]]
+# then
+#     ctmid=$(xinput -list-props $tbnid | grep Matrix |  sed -r 's/.* Matrix \((\S+)\).*/\1/')
+# elif [[ -z "$tbnid" ]] && [[ ! -z "$tbnid1" ]]
+# then
+#     ctmid=$(xinput -list-props $tbnid1 | grep Matrix |  sed -r 's/.* Matrix \((\S+)\).*/\1/')
+# else
+#     xinput -set-prop $tbnid $ctmid $uin 0 0 0 $uin 0 0 0 1
+#     notify-send "Current Mouse Sensitivity: $uin"
+# fi    
+
+
+# xinput -set-prop 13 167 0.5 0 0 0 0.5 0 0 0 1 <- for thumb trackball
